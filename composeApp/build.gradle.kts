232e41956a015e3a0d143a17df195d609e735ca5
sepidehAkbarinezhad <sepid_ak@yahoo.com>
2025-02-24 10:26:02 +0330

    add room dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 kotlin {
@@ -51,6 +53,8 @@ kotlin {
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
             api(libs.koin.core)
+            implementation(libs.androidx.room.runtime)
+            implementation(libs.sqlite.bundled)
         }
     }
 }
@@ -85,4 +89,9 @@ android {
 dependencies {
     debugImplementation(compose.uiTooling)
 }
+
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
 
