4cbe8e6c997bd232a4c5542c1eb07e9233711205
sepidehAkbarinezhad <sepid_ak@yahoo.com>
2025-02-24 11:29:04 +0330

    add room classes

---
+++ composeApp/src/androidMain/kotlin/com/sepideh/lilo/database/getTaskDatabaseBuilder.kt
@@ -0,0 +1,15 @@
+package com.sepideh.lilo.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+
+fun getDatabaseBuilder(ctx : Context) : RoomDatabase.Builder<TaskDatabase>{
+    val appContext = ctx.applicationContext
+    val dbFile = appContext.getDatabasePath("task.db")
+    return Room.databaseBuilder<TaskDatabase>(
+        context = appContext,
+        name = dbFile.absolutePath
+    ).setDriver(BundledSQLiteDriver())
+}
