9fa474005099c6cfee2f96ddd62e157108352b03
sepideh.akbarinezhad <sepid_ak@yahoo.com>
2025-03-05 09:15:12 +0330

    update room version

--- composeApp/src/commonMain/kotlin/com/sepideh/lilo/task/data/TaskDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/sepideh/lilo/task/data/TaskDatabase.kt
@@ -1,12 +1,21 @@
 package com.sepideh.lilo.task.data
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 
 @Database(
     entities = [TaskEntity::class],
     version = 1
 )
+@ConstructedBy(DbConstructor::class)
 abstract class TaskDatabase : RoomDatabase(){
     abstract fun taskDao(): TaskDao
+}
+
+// The Room compiler generates the `actual` implementations.
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object DbConstructor: RoomDatabaseConstructor<TaskDatabase> {
+    override fun initialize(): TaskDatabase
 }
