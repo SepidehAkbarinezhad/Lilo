57693c38156e87dc47d7bdfb7adcad9ae1252d0d
sepideh.akbarinezhad <sepid_ak@yahoo.com>
2025-04-30 11:26:50 +0330

    remove reminder manager

--- composeApp/src/commonMain/kotlin/com/sepideh/lilo/task/domain/reminder/TimeManager.kt
+++
@@ -1,4 +0,0 @@
-package com.sepideh.lilo.task.domain.reminder
-
-
-expect fun setReminderTime(dayMillis: Long?, hour: Int?, minute: Int?) : Long?
--- composeApp/src/iosMain/kotlin/com/sepideh/lilo/task/domain/reminder/ReminderManager.ios.kt
+++
@@ -1,6 +0,0 @@
-package com.sepideh.lilo.task.domain.reminder
-
-
-actual fun setReminderTime(dayMillis: Long?, hour: Int?, minute: Int?) : Long? {
-    return null
-}
