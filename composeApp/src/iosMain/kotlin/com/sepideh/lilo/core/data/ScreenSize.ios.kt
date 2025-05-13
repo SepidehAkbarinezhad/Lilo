0517fae7a504fc737a1456797ac81ea23b8b2b4d
sepidehAkbarinezhad <sepid_ak@yahoo.com>
2025-05-13 12:09:32 +0330

    comment screen size

--- composeApp/src/iosMain/kotlin/com/sepideh/lilo/core/data/ScreenSize.ios.kt
+++ composeApp/src/iosMain/kotlin/com/sepideh/lilo/core/data/ScreenSize.ios.kt
@@ -1,9 +1,7 @@
 package com.sepideh.lilo.core.data
 
 actual object ScreenSize {
-    actual val widthDp: Float
-        get() = UIScreen.mainScreen.bounds.useContents { size.width.toFloat() }
 
     actual val heightDp: Float
-        get() = UIScreen.mainScreen.bounds.useContents { size.height.toFloat() }
+        get() = 600f
 }
