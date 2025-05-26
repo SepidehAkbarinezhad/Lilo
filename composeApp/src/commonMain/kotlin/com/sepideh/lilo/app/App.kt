3cf84575ab2f9bedc70129812f51df9c5791eb53
sepidehAkbarinezhad <sepid_ak@yahoo.com>
2025-05-26 15:11:10 +0330

    hide system status

--- composeApp/src/commonMain/kotlin/com/sepideh/lilo/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/sepideh/lilo/app/App.kt
@@ -1,6 +1,15 @@
 package com.sepideh.lilo.app
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.sepideh.lilo.app.navigation.NavigationGraph
 import com.sepideh.lilo.ui.theme.LiloTheme
@@ -8,13 +17,19 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
 @Composable
-@Preview
 fun App(
     darkTheme: Boolean,
     dynamicColor: Boolean
 ) {
     LiloTheme(darkTheme = darkTheme, dynamicColor = dynamicColor){
-        val navHostController = rememberNavController()
-        NavigationGraph(navHostController = navHostController)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(WindowInsets.systemBars.asPaddingValues())
+        ) {
+            val navHostController = rememberNavController()
+            NavigationGraph(navHostController = navHostController)
+        }
     }
 }
