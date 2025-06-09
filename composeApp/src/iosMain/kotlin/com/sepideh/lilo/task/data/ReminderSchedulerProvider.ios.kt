b37d3b012c969c09ff6d5174cb4a123846a81ff9
sepidehAkbarinezhad <sepid_ak@yahoo.com>
2025-06-09 12:11:46 +0330

    debug showing notification in ios

--- composeApp/src/iosMain/kotlin/com/sepideh/lilo/task/data/ReminderSchedulerProvider.ios.kt
+++ composeApp/src/iosMain/kotlin/com/sepideh/lilo/task/data/ReminderSchedulerProvider.ios.kt
@@ -6,10 +6,15 @@ import platform.Foundation.NSCalendarUnitDay
 import platform.Foundation.NSCalendarUnitHour
 import platform.Foundation.NSCalendarUnitMinute
 import platform.Foundation.NSCalendarUnitMonth
+import platform.Foundation.NSCalendarUnitSecond
 import platform.Foundation.NSCalendarUnitYear
 import platform.Foundation.NSDate
 import platform.Foundation.NSDateComponents
+import platform.Foundation.NSTimeZone
 import platform.Foundation.dateWithTimeIntervalSince1970
+import platform.Foundation.localTimeZone
+import platform.UserNotifications.UNAuthorizationOptionAlert
+import platform.UserNotifications.UNAuthorizationOptionSound
 import platform.UserNotifications.UNCalendarNotificationTrigger
 import platform.UserNotifications.UNMutableNotificationContent
 import platform.UserNotifications.UNNotificationRequest
@@ -17,16 +22,30 @@ import platform.UserNotifications.UNNotificationSound
 import platform.UserNotifications.UNUserNotificationCenter
 
 
- class ReminderSchedulerProvider : ReminderScheduler {
-     override fun scheduleReminder(reminder: Reminder) {
-         reminder.startDate?.let { start ->
-             if (reminder.endDate != null) {
-                 scheduleRangeReminders(reminder)
-             } else {
-                 scheduleSingleReminder(reminder, start)
-             }
-         }
+class ReminderSchedulerProvider : ReminderScheduler {
+    private val notificationDelegate = NotificationDelegate()
+    init {
+        UNUserNotificationCenter.currentNotificationCenter()
+            .requestAuthorizationWithOptions(
+                options = UNAuthorizationOptionAlert or UNAuthorizationOptionSound
+            ) { granted, error ->
+                println("Notification permission granted: $granted, error: $error")
+            }
+
+        // Set delegate to show notifications in foreground
+        UNUserNotificationCenter.currentNotificationCenter().delegate = notificationDelegate
     }
+
+    override fun scheduleReminder(reminder: Reminder) {
+        reminder.startDate?.let { start ->
+            if (reminder.endDate != null) {
+                scheduleRangeReminders(reminder)
+            } else {
+                scheduleSingleReminder(reminder, start)
+            }
+        }
+    }
+
     private fun scheduleSingleReminder(reminder: Reminder, timestampMillis: Long) {
         val dateComponents = timestampMillis.toNSDateComponents()
         val content = createNotificationContent(reminder)
@@ -42,9 +61,10 @@ import platform.UserNotifications.UNUserNotificationCenter
             trigger = trigger
         )
 
-        UNUserNotificationCenter.currentNotificationCenter().addNotificationRequest(request) { error ->
-            error?.let { println("iOS: Failed to schedule single reminder: $it") }
-        }
+        UNUserNotificationCenter.currentNotificationCenter()
+            .addNotificationRequest(request) { error ->
+                error?.let { println("iOS: Failed to schedule single reminder: $it") }
+            }
     }
 
     private fun scheduleRangeReminders(reminder: Reminder) {
@@ -67,9 +87,10 @@ import platform.UserNotifications.UNUserNotificationCenter
                 trigger = trigger
             )
 
-            UNUserNotificationCenter.currentNotificationCenter().addNotificationRequest(request) { error ->
-                error?.let { println("iOS: Failed to schedule range reminder: $it") }
-            }
+            UNUserNotificationCenter.currentNotificationCenter()
+                .addNotificationRequest(request) { error ->
+                    error?.let { println("iOS: Failed to schedule range reminder: $it") }
+                }
 
             // Increment by 1 day
             current += 24 * 60 * 60 * 1000
@@ -106,9 +127,14 @@ import platform.UserNotifications.UNUserNotificationCenter
 
 fun Long.toNSDateComponents(): NSDateComponents {
     val calendar = NSCalendar.currentCalendar
+    calendar.timeZone = NSTimeZone.localTimeZone // 👈 Use local time zone
+
     val date = NSDate.dateWithTimeIntervalSince1970(this / 1000.0)
-    return calendar.components(
-        NSCalendarUnitYear or NSCalendarUnitMonth or NSCalendarUnitDay or NSCalendarUnitHour or NSCalendarUnitMinute,
+    val components = calendar.components(
+        NSCalendarUnitYear or NSCalendarUnitMonth or NSCalendarUnitDay or
+                NSCalendarUnitHour or NSCalendarUnitMinute or NSCalendarUnitSecond,
         fromDate = date
     )!!
+    components.setTimeZone(NSTimeZone.localTimeZone)
+    return components
 }
