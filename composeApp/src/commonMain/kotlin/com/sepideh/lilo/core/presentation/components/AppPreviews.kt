ac09cd83fc8b614b45f83e1704685cb8d69cab37
sepidehAkbarinezhad <sepid_ak@yahoo.com>
2026-07-30 21:47:44 +0330

    change LiloTheme  and AppPreview

--- composeApp/src/commonMain/kotlin/com/sepideh/lilo/core/presentation/components/AppPreviews.kt
+++ composeApp/src/commonMain/kotlin/com/sepideh/lilo/core/presentation/components/AppPreviews.kt
@@ -2,10 +2,10 @@ package com.sepideh.lilo.core.presentation.components
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_NO
 import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
 import androidx.compose.ui.tooling.preview.Preview
-import com.sepideh.lilo.core.utils.LocalLanguageCode
 import com.sepideh.lilo.ui.theme.LiloTheme
 
 // 1. Compile-time constants for the @Preview locale parameters
@@ -21,15 +21,16 @@ object PreviewLocales {
 @Preview(locale = PreviewLocales.EN, uiMode = UI_MODE_NIGHT_YES)
 annotation class AppPreviews
 
-
 @Composable
 fun LiloPreviewWrapper(
     content: @Composable () -> Unit
 ) {
-    val languageCode = LocalLanguageCode.current // Respects the preview's locale automatically
     val darkTheme = isSystemInDarkTheme()
 
-    LiloTheme(darkTheme = darkTheme, languageCode = languageCode) {
+    LiloTheme(
+        darkTheme = darkTheme,
+        layoutDirection =  LocalLayoutDirection.current
+    ) {
         content()
     }
 }
--- composeApp/src/commonMain/kotlin/com/sepideh/lilo/ui/theme/LiloTheme.kt
+++ composeApp/src/commonMain/kotlin/com/sepideh/lilo/ui/theme/LiloTheme.kt
@@ -1,7 +1,6 @@
 package com.sepideh.lilo.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
@@ -13,22 +12,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import com.sepideh.lilo.core.domain.model.AppTheme
 import com.sepideh.lilo.core.utils.LanguageUtils
-import com.sepideh.lilo.core.utils.LocalLanguageCode
 import com.sepideh.lilo.core.utils.changeLanguage
 import com.sepideh.lilo.settings.domain.model.UserPreferences
 import com.sepideh.lilo.settings.domain.usecase.UserPreferencesManager
-import com.sepideh.lilo.settings.presentation.model.AppTheme
 import org.koin.mp.KoinPlatform.getKoin
 
 val DarkColorScheme = darkColorScheme(
     background = Color(0xFF121212),
-    surface = Grey900,
-    surfaceVariant = Grey800,
+    surface = Gray900,
+    surfaceVariant = Gray800,
     surfaceContainer = BlueGray900,
     primary = Amber600,
     onPrimary = White,
-    primaryContainer = Grey800,
+    primaryContainer = Gray800,
     secondary = Blue600,
 )
 val LightColorScheme = lightColorScheme(
@@ -36,7 +35,7 @@ val LightColorScheme = lightColorScheme(
     surface = White,
     onSurface = Gray600,
     surfaceVariant = Color(0x0DFFAB00),
-    onSurfaceVariant = Grey900,
+    onSurfaceVariant = Gray900,
     surfaceContainer = White,
     primary = Amber600,
     onPrimary = White,
@@ -44,7 +43,11 @@ val LightColorScheme = lightColorScheme(
     secondary = Blue600,
 )
 
-// --- Real app entry point: reads live UserPreferences from Koin ---
+/*
+* Real app entry point:
+* reads live UserPreferences from Koin, run side effects,changeLanguage() updates platform process resources at runtime ...
+* so cant be called from preview
+* */
 @Composable
 internal fun LiloTheme(
     content: @Composable () -> Unit
@@ -58,24 +61,28 @@ internal fun LiloTheme(
         AppTheme.SYSTEM -> isSystemInDarkTheme()
     }
 
+    val languageCode = userPreferences.language.code
+    LaunchedEffect(languageCode) {
+        changeLanguage(languageCode)
+    }
+    val layoutDirection = remember(languageCode) {
+        LanguageUtils.layoutDirection(languageCode)
+    }
+
     LiloTheme(
         darkTheme = darkTheme,
-        languageCode = userPreferences.language.code,
+        layoutDirection = layoutDirection,
         content = content
     )
 }
 
-// --- Pure, parameterized version: used by previews & tests, no Koin needed ---
+//  Pure, parameterized version: used by previews & tests, no Koin needed, no runtime process
 @Composable
 internal fun LiloTheme(
     darkTheme: Boolean,
-    languageCode: String,
+    layoutDirection: LayoutDirection,
     content: @Composable () -> Unit
 ) {
-
-    val layoutDirection = remember(languageCode) {
-        LanguageUtils.layoutDirection(languageCode)
-    }
 
     val liloColorsPalette = if (darkTheme) LiloColorsDark else LiloColorsLight
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
@@ -84,7 +91,6 @@ internal fun LiloTheme(
     CompositionLocalProvider(
         LocalLiloColorsPalette provides liloColorsPalette,
         LocalLayoutDirection provides layoutDirection,
-        LocalLanguageCode provides languageCode
     ) {
         MaterialTheme(
             colorScheme = colorScheme,
@@ -94,7 +100,7 @@ internal fun LiloTheme(
     }
 }
 
-object LiloAppTheme {
-    val liloColor: LiloColors
+object LiloExtendedTheme {
+    val colors: LiloColors
         @Composable get() = LocalLiloColorsPalette.current
 }
